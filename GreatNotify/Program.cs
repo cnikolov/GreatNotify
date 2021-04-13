949e4f4af01d39b9054ee3637844b2d051ab438d
Christian Nikolov <christian.nikolov@outlook.com>
2021-04-13 16:30:56 +0100

    Add project files.

---
+++ GreatNotify/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GreatNotify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
